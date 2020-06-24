9bba9d1635e27a44e07041970508a73d65b836af
Tommy Thorn <tommy-github2@thorn.ws>
2020-06-24 14:42:28 -0700

    [fpga] Get YARVI2 building for OrangeCrab and check speed
    Currently we aren't even meeting 48 MHz.  Much to change.

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
+all:  check-speed run-tests
+	grep MHz target/OrangeCrab/nextpnr-ecp5.out|tail -1
+
 run-tests:
 	$(MAKE) -s -C sw/rv32-compliance
 	$(MAKE) -s -C sw/rv32-tests
+	@echo "Expect 46 passing"
 #	$(MAKE) -s -C sw/hello_world
 
-fpgademo:
-	$(MAKE) -C target/BeMicroCV-A9
+check-speed:
+	-$(MAKE) -C target/OrangeCrab top_out.config
--- target/OrangeCrab/top.v
+++ target/OrangeCrab/top.v
@@ -1,3 +1,5 @@
+`default_nettype none
+(* top *)
 module top
   (input  wire      clk48,
    output reg [2:0] led = 0,
@@ -5,33 +7,33 @@ module top
    input wire       rx,
    output wire      tx);
 
-   reg [2:0]        led_color = 7;
-   reg              led_on    = 1;
-
-   //  48e6 + 40% ~= 2^26
-   reg [26:0]       clk_div = 0;
-
-   wire             tx_data_ready;
    wire             tx_data_valid;
+   wire             tx_data_ready;
    wire [7:0]       tx_data;
 
-   wire             rx_ready; // Ignored, might drop RX data
    wire             rx_data_valid;
+   wire             rx_data_ready;
    wire [7:0]       rx_data;
 
    rs232out #(115200, 48000000) tx_inst(clk48, tx, tx_data_valid, tx_data_ready, tx_data);
    rs232in  #(115200, 48000000) rx_inst(clk48, rx, rx_data_valid, rx_data);
 
+   wire [31:0]      me_pc;
+   always @(posedge clk48) led <= led + ^me_pc;
+
    yarvi_soc yarvi_soc
      ( .clock           (clk48)
      , .reset           (0)
 
-     , .rx_ready        (rx_ready)
-     , .rx_valid        (rx_valid)
+     , .rx_ready        (rx_data_ready)
+     , .rx_valid        (rx_data_valid)
      , .rx_data         (rx_data)
 
-     , .tx_ready        (tx_ready)
-     , .tx_valid        (tx_valid)
+     , .tx_ready        (tx_data_ready)
+     , .tx_valid        (tx_data_valid)
      , .tx_data         (tx_data)
+
+     , .me_pc           (me_pc)
      );
+
 endmodule
